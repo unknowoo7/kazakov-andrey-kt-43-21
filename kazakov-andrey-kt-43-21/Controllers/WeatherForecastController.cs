bbe70c2db17d03ba1f7e17e5ab3b101928fc77cf
unknowoo7 <kazak1995kazak@yandex.ru>
2024-09-05 19:31:16 +0300

    [laba_02] End laba 02. Add logger

--- kazakov-andrey-kt-43-21/Controllers/WeatherForecastController.cs
+++ kazakov-andrey-kt-43-21/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace kazakov_andrey_kt_43_21.Controllers
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+      _logger.LogError("Method was called!");
       return Enumerable.Range(1, 5).Select(index => new WeatherForecast
       {
         Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -28,6 +29,16 @@ namespace kazakov_andrey_kt_43_21.Controllers
         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
       })
       .ToArray();
+    }
+
+
+    [HttpPost(Name = "AddNewSummary")]
+    public string[] AddNewSummary(string newSummary)
+    {
+      _logger.LogError("Method was called!");
+      var list = Summaries.ToList();
+      list.Add(newSummary);
+      return list.ToArray();
     }
   }
 }
