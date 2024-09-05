f2cdc81ce519e958c374b27f23f4f0060eece51f
unknowoo7 <kazak1995kazak@yandex.ru>
2024-09-05 18:44:45 +0300

    [laba_01] Initial solusion

---
+++ kazakov-andrey-kt-43-21/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace kazakov_andrey_kt_43_21
+{
+  public class WeatherForecast
+  {
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+  }
+}
