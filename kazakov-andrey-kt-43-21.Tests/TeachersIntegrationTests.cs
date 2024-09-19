8b5a6e4e9f23235e255501bf28e38b99fa3a73fb
unknowoo7 <kazak1995kazak@yandex.ru>
2024-09-19 20:17:49 +0300

    [laba_06] Add tests

--- kazakov-andrey-kt-43-21.Tests/TeachersIntegrationTests.cs
+++ kazakov-andrey-kt-43-21.Tests/TeachersIntegrationTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using kazakov_andrey_kt_43_21.Database;
+using kazakov_andrey_kt_43_21.Interfaces.StudentsInterfaces;
+using kazakov_andrey_kt_43_21.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,66 @@ using System.Threading.Tasks;
 
 namespace kazakov_andrey_kt_43_21.Tests
 {
-  internal class TeachersIntegrationTests
+  public class TeachersIntegrationTests
   {
+    public readonly DbContextOptions<TeacherDbContext> _dbContextOptions;
+    
+    public TeachersIntegrationTests()
+    {
+      _dbContextOptions = new DbContextOptionsBuilder<TeacherDbContext>()
+        .UseInMemoryDatabase(databaseName: "teacher_db")
+        .Options;
+    }
+
+    [Fact]
+    public async Task GetTeachersByDepartmentAsync_IT_TwoObject()
+    {
+      var ctx = new TeacherDbContext(_dbContextOptions);
+      var teacherService = new TeacherService(ctx);
+      var departments = new List<Department>
+      {
+        new Department
+        {
+          DepartmentName = "Математика"
+        },
+        new Department
+        {
+          DepartmentName = "Химия"
+        }
+      };
+
+      await ctx.Set<Department>().AddRangeAsync(departments);
+
+      var teachers = new List<Teacher>
+      {
+        new Teacher
+        {
+          FirstName = "Иван",
+          LastName = "Иванов",
+          MiddleName = "Иванович",
+          DepartmentId = 1,
+        },
+        new Teacher
+        {
+          FirstName = "Петр",
+          LastName = "Петров",
+          MiddleName = "Петрович",
+          DepartmentId = 1,
+        }
+      };
+
+      await ctx.Set<Teacher>().AddRangeAsync(teachers);
+
+      await ctx.SaveChangesAsync();
+
+      var filter = new Filters.TeacherFilters.TeacherDepartmentFilter
+      {
+        DepartmentName = "Математика"
+      };
+
+      var teachersResult = await teacherService.GetTeachersByDepartmentAsync(filter, CancellationToken.None);
+
+      Assert.Equal(2, teachersResult.Length);
+    }
   }
 }
