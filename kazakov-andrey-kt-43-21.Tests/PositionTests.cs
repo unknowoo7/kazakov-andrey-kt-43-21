e493b64df2506fae2da8d90cdead150eca893391
unknowoo7 <kazak1995kazak@yandex.ru>
2024-11-18 10:04:51 +0300

    Add test xunit

---
+++ kazakov-andrey-kt-43-21.Tests/PositionTests.cs
@@ -0,0 +1,28 @@
+﻿using kazakov_andrey_kt_43_21.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kazakov_andrey_kt_43_21.Tests
+{
+  public class PositionTests
+  {
+    [Fact]
+    public void IsValidPositionName_KT4321_True()
+    {
+      //arrange
+      var test = new Position
+      {
+        PositionName = "Химия"
+      };
+
+      //act
+      var result = test.isValidPositionName();
+
+      //assert
+      Assert.True(result);
+    }
+  }
+}
